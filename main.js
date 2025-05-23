cb64b188e7bcd34a9380463fb2c74e20e549043f
rob <roby.pakalns@gmail.com>
2025-05-23 17:44:58 +0300

    init

---
+++ main.js
@@ -0,0 +1,30 @@
+const TABLE = SpreadsheetApp.getActiveSpreadsheet()
+const LENGTH = 531
+
+const sheets = {
+  main: TABLE.getSheetByName("Main"),
+  history: TABLE.getSheetByName("History")
+}
+
+const getData = url => JSON.parse(UrlFetchApp.fetch(url).getContentText())
+
+const init = () => {
+  const values = []
+
+  for (let i = 1; i <= LENGTH; i++) {
+    console.log("fetching... " + i)
+    const data = getData(`https://api.tricko.pro/voxiom/market/skin/${i}`)
+    const value = processData(data.data.price_history)
+    values.push([value])
+
+    Utilities.sleep(1000)
+  }
+
+  const { history } = sheets
+  const nextCol = history.getLastColumn() + 1
+
+  const date = new Date().toLocaleDateString("en-US")
+  history.getRange(1, nextCol).setValue(date)
+
+  history.getRange(2, nextCol, values.length, 1).setValues(values)
+}
---
+++ processing.js
@@ -0,0 +1,21 @@
+// Calculates the p-th percentile of a sorted array using linear interpolation
+const percentile = (arr, p) => {
+    if (arr.length === 0) return NaN
+
+    const index = (p / 100) * (arr.length - 1)
+    const lower = Math.floor(index)
+    const weight = index % 1
+
+    return weight === 0 ? arr[lower] : arr[lower] * (1 - weight) + arr[lower + 1] * weight
+}
+
+// Advanced average price calculation; removes outliers
+const processData = data => {
+    const prices = data.map(el => el.price).sort((a, b) => a - b)
+    const filteredPrices = prices.filter(i => i >= percentile(prices, 50) * 0.5)
+    const Q1 = percentile(filteredPrices, 25)
+    const Q3 = percentile(filteredPrices, 75)
+    const IQR = Q3 - Q1
+    const result = filteredPrices.filter(el => el >= Q1 - 1.5 * IQR && el <= Q3 + 1.5 * IQR)
+    return result.length === 0 ? "" : (result.reduce((acc, el) => acc + el, 0) / result.length).toFixed(0)
+}
